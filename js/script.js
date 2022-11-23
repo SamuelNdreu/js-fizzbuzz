f81b6caf33c339e1b3f280a75313cbe0ee39366f
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-23 16:13:38 +0100

    finished

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ for (let i = 0; i < 101; i++){
     console.log(i);
 
     if (i % 3 === 0){
+        frizz.append("Fizz! ");
         console.log(i + 'frizz')
     }else if (i % 5 === 0){
+        buzz.append("Buzz! ");
         console.log(i + 'buzz');
     }
     if (i % 3 === 0 && i % 5 === 0){
+        fizzBuzz.append("FrizzBuzz! ");
         console.log(i + 'FrizzBuzz');
     }
 }
