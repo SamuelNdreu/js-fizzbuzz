308e37c4aeeeab88fdeeebba4bd98e8eac7d4c6c
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-23 15:52:00 +0100

    milestone one ready

--- js/script.js
+++ js/script.js
@@ -4,22 +4,15 @@ let fizzBuzz = document.getElementById("frizzbuzz");
 let numbers = document.getElementById("numbers");
 
 //First Step: scrivi un programma che stampi in console i numeri da 0 a 100
-for (let i = 0; i <= 100; i++) {
-    if (i % 15 === 0) {
+for (let i = 0; i < 101; i++){
+    console.log(i);
 
-    //? Per i numeri che sono multipli di 3 che di 5 stampi <"FizzBuzz">
-    // fizzBuzz.append("FizzBuzz! ");
-    console.log("FrizzBuzz");
-    
-} else if (i % 3 === 0) {
-    //?per i multiply di 5 stampi <"Fizz">
-    // fizz.append("Fizz! ");
-    console.log("Frizz");
-
-} else if (i % 5 === 0) {
-    //? per i multipli di 5 stampi “Buzz”
-    // buzz.append("Buzz! ");
-    console.log("Buzz");
-    console.log(i + 'buzz')
-}
+    if (i % 3 === 0){
+        console.log(i + 'frizz')
+    }else if (i % 5 === 0){
+        console.log(i + 'buzz');
+    }
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(i + 'FrizzBuzz');
+    }
 }
